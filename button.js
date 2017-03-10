cf2645f1060097672475322daac445cab7f11b18
tm2 <tm@personal-media.co.jp>
2017-03-10 11:47:14 +0900

    fs.writeFile

--- button.js
+++ button.js
@@ -133,9 +133,9 @@ controller.on('interactive_message_callback', function(bot, message) {
     var text = ""
 
     if (name == "good") {
-      text = "That's good 2."
+      text = "That s good 3."
     } else {
-      text = "What's wrong? 2"
+      text = "What s wrong? 3"
     }
 
 var data = {
